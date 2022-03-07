eceb9e4e2b0fe3316d94030c382a4b6883affac6
Ben331J <101118770+Ben331J@users.noreply.github.com>
2022-03-07 23:41:45 +0100

    Main input Regex verification pour the square input Original square display redo

--- sudoku-resolver.py
+++ sudoku-resolver.py
@@ -1,40 +1,80 @@
+# This program will resolve a sudoku
+
+# Module importation
 import re
 
-def length_check(square):
-	input_length = len(square)
-	if input_length != 9:
-		print("The square must have 9 elements")
-		print("You have entered " + str(input_length) + " elements")
-		return False
-	else:
+
+
+# Function to verify if the user input has 9 digit or space
+# Return true if it's correct or false if not
+def regex_verify(square):
+	regex = r"^[\d\s]{9}$"
+
+	if re.search(regex, square):
 		return True
 
+	else:
+		print("The square must have 9 digits or white-spaces")
+		return False
+
+
+
+# Program start
 print("Welcome to my sudoku resolver")
-print("Use only numbers [1-9] and space keys")
 
-square_check = False
-while square_check != True:
-	square = input("Square : ")
 
-	if length_check(square):
-		square_check = True
+main_input_list = ("N", "E")
 
-if square_check == True:
-	i = 0
-	line_1 = ""
-	line_2 = ""
-	line_3 = ""
-	while i < 3:
-		line_1 += square[i]
-		i += 1
-	while i < 6:
-		line_2 += square[i]
-		i += 1
-	while i < 9:
-		line_3 += square[i]
-		i += 1
+user_input = ""
 
-	print("The square is :")
-	print(line_1)
-	print(line_2)
-	print(line_3)
+while user_input not in main_input_list:
+	print("Press N to enter a new square")
+	print("Press E to exit")
+	user_input = input("? : ")
+
+if user_input == "N":
+	print("Use only digits [1-9] and white-spaces to enter a new square")
+
+	# Ask user for square input
+	square_correct = False
+
+	# Input verify
+	while square_correct != True:
+		
+		original_square = input("Original square : ")
+		
+		if regex_verify(original_square):
+			square_correct = True
+
+	# If the user input is correct
+	# The original square is displayed
+	if square_correct == True:
+		
+		count = 0
+		line_count = 0
+		box_count = 0
+		line = ""
+		lines = []
+
+		while line_count < 3:
+			line += original_square[box_count]
+			count += 1
+			box_count += 1
+
+			if count == 3:
+				lines.append(line)
+				count = 0
+				line_count += 1
+				line = ""
+
+		# Display the square
+		print("The square is :")
+		for x in lines:
+			print(x)
+
+	resolve_input_list = ("R", "E")
+
+	input("Press R to resolve :")
+
+if user_input == "E":
+	print("Bye")
