ef2d04280abdf62c2eb4d7f170da2a24d36704c5
Ben331J <101118770+Ben331J@users.noreply.github.com>
2022-03-10 11:28:00 +0100

    Add timer

--- sudoku-solver.py
+++ sudoku-solver.py
@@ -1,9 +1,13 @@
+import time
 from functions import *
 from grids import *
 
 grid = rand_select(grids)
 print("Original board")
 console_display(grid)
+start = time.perf_counter_ns()
 grid = solve(grid)
-print("Solved grid")
+stop = time.perf_counter_ns()
+time = (stop - start) / 10 ** 6
+print(f"Solved grid in {time} ms")
 console_display(grid)
